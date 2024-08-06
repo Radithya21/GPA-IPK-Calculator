8298218020c86da4d0c03206468bd59dbe76c5d7
[Nuril21] <dimasdrn21@gmail.com>
2024-08-06 19:41:36 +0700

    commit pertama

---
+++ Menghitung IPK/.vscode/IPK.py
@@ -0,0 +1,12 @@
+
+# menghitung ipk
+
+jumlah_ip = 0
+jumlah_sem = int(input("sudah semester berapa kamu? "))
+
+for i in range(1,jumlah_sem+1):
+    ip = float(input(f"Silahkan masukan ip semester {i}: "))
+    jumlah_ip += ip
+
+mean = jumlah_ip/jumlah_sem
+print(f"IPK kamu selama {jumlah_sem} semester adalah {mean:.2f}")
